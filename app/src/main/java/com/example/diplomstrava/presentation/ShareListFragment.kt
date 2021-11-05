0bcd2a0f732cacdcdda6b5400e9a14bdba59f25d
Алябьев Андрей <aluaba26@gmail.com>
2021-11-05 08:51:20 +0300

    share list completed

---
+++ app/src/main/java/com/example/diplomstrava/presentation/ShareListFragment.kt
@@ -0,0 +1,7 @@
+package com.example.diplomstrava.presentation
+
+import androidx.fragment.app.Fragment
+import com.example.diplomstrava.R
+
+class ShareListFragment: Fragment(R.layout.fragment_share_list) {
+}
